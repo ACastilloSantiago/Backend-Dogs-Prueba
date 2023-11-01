07fb60000a97795afd9df4098bd24a9d76dd4e27
ACastilloSantiago <santiagorodrigec@gmail.com>
2023-11-01 19:34:42 -0300

    Backend-dogs-prueba

---
+++ index.js
@@ -0,0 +1,29 @@
+//                       _oo0oo_
+//                      o8888888o
+//                      88" . "88
+//                      (| -_- |)
+//                      0\  =  /0
+//                    ___/`---'\___
+//                  .' \\|     |// '.
+//                 / \\|||  :  |||// \
+//                / _||||| -:- |||||- \
+//               |   | \\\  -  /// |   |
+//               | \_|  ''\---/''  |_/ |
+//               \  .-\__  '-'  ___/-. /
+//             ___'. .'  /--.--\  `. .'___
+//          ."" '<  `.___\_<|>_/___.' >' "".
+//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+//         \  \ `_.   \_ __\ /__ _/   .-` /  /
+//     =====`-.____`.___ \_____/___.-`___.-'=====
+//                       `=---='
+//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+const server = require("./src/app.js");
+const { conn } = require("./src/db.js");
+const getTemperaments = require("./src/controllers/getTemperaments.js");
+const port = process.env.PORT || 3001;
+conn.sync({ force: true }).then(() => {
+  server.listen(port, () => {
+    getTemperaments();
+    console.log(`%s listening at ${port}`); // eslint-disable-line no-console
+  });
+});
