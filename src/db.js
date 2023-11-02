5fa7e2b313ba486cda9c8ce943d2d6f2fa99aa86
ACastilloSantiago <santiagorodrigec@gmail.com>
2023-11-02 11:30:43 -0300

    pg

--- src/db.js
+++ src/db.js
@@ -3,13 +3,13 @@ const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_DATABASE } = process.env;
-
+const pg = require("pg");
 const sequelize = new Sequelize(
   `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-    // dialectModule: pg,
+    dialectModule: pg,
   }
 );
 const basename = path.basename(__filename);
